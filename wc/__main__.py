b8672f561798f6a9e311b5df5ab43c3ec94ac125
programmerxero <programmerxero@gmail.com>
2024-12-21 00:30:10 +0330

    add total functionality

--- wc/__main__.py
+++ wc/__main__.py
@@ -2,6 +2,8 @@ import argparse
 from os.path import exists, isdir
 from sys import argv
 
+PROGRAM_NAME = argv[0]
+
 def hex_escape(s):
     from string import ascii_letters, digits, punctuation
     printable = ascii_letters + digits + punctuation + " "
@@ -10,9 +12,9 @@ def hex_escape(s):
 def get_path_from_file(path):
     file_list = []
     if not exists(path):
-        print(f"{argv[0]}: cannot open '{path}' for reading: No such file or directory")
+        print(f"{PROGRAM_NAME}: cannot open '{path}' for reading: No such file or directory")
     elif isdir(path):
-        print(f"{argv[0]}: {path}: read error: Is a directory")
+        print(f"{PROGRAM_NAME}: {path}: read error: Is a directory")
     with open(path, "r") as file:
         data = file.read()
         curr_path = ""
@@ -33,7 +35,7 @@ def get_file_details(path):
                "chars": 0,
                "lines": 0,
                "max_line_length": 0,
-               "words": 0}
+               "words": 0,}
     with open(path, "r") as file:
         line_length = 0
         is_in_word = False
@@ -61,12 +63,15 @@ def get_file_details(path):
 
 def get_stdin_details():
     from sys import stdin
-    data = stdin.read()
     details = {"bytes": 0,
                "chars": 0,
                "lines": 0,
                "max_line_length": 0,
-               "words": 0}
+               "words": 0,}
+    try:
+        data = stdin.read()
+    except KeyboardInterrupt:
+        return None
     line_length = 0
     is_in_word = False
     for char in data:
@@ -126,6 +131,7 @@ def main():
     parser.add_argument("-L", "--max-line-length", action="store_true", help="print the maximum display width")
     parser.add_argument("-w", "--words", action="store_true", help="print the word counts")
     parser.add_argument("--files0-from", type=str, help="read input from the files specified by NUL-terminated names in file F; If F is - then read names from standard input")
+    parser.add_argument("--total", type=str, help="when to print a line with total counts; WHEN can be: auto, always, only, never")
     # parse arguments
     args = parser.parse_args()
 
@@ -134,7 +140,16 @@ def main():
         print("v0.3.0")
         print("Based on the tool written by Paul Rubin and David MacKenzie.")
         quit(0)
-    
+    # total options
+    total_values = ["auto", "always", "only", "never"]
+    if args.total and args.total not in total_values:
+        print(f"{PROGRAM_NAME}: invalid argument ‘{args.total}’ for ‘--total’")
+        print("Valid arguments are:")
+        for v in total_values:
+            print(f" - ‘{v}’")
+        print(f"Try '{PROGRAM_NAME} --help' for more information.")
+        quit(0)
+
     files = args.FILE # extract FILE
 
     program_state = get_state(args) # program state
@@ -143,30 +158,66 @@ def main():
     if args.files0_from is not None:
         if len(files) > 0:
             print("file operands cannot be combined with --files0-from")
-            print(f"Try '{argv[0]} --help' for more information.")
+            print(f"Try '{PROGRAM_NAME} --help' for more information.")
             quit(0)
         files = get_path_from_file(args.files0_from)
     
     # no file
     if len(files) == 0:
-        report(program_state, get_stdin_details(), "", sep="\t")
+        details = get_stdin_details()
+        if details:
+            report(program_state, details, "", sep="\t")
         quit(0)
     
+    # total details
+    total_details = {"bytes": 0,
+                    "chars": 0,
+                    "lines": 0,
+                    "max_line_length": 0,
+                    "words": 0,}
+
     # with FILE
     if len(files) > 0:
+        details = {}
         for path in files:
             if path == "-":
-                report(program_state, get_stdin_details(), path)
+                details = get_stdin_details()
+                if details is None:
+                    quit(0) 
+                else:
+                    if args.total != "only":
+                        report(program_state, details, path)
+                # add to total
+                for k, v in details.items():
+                    if k == "max_line_length":
+                        total_details[k] = max(total_details[k], v)
+                    else:
+                        total_details[k] += v
+                    details = {}
                 continue
             # check file validity
             if not exists(path):
-                print(f"{argv[0]}: {hex_escape(path)}: No such file or directory")
+                print(f"{PROGRAM_NAME}: {hex_escape(path)}: No such file or directory")
                 continue
             if isdir(path):
-                print(f"{argv[0]}: {hex_escape(path)}: Is a directory")
+                print(f"{PROGRAM_NAME}: {hex_escape(path)}: Is a directory")
                 continue
- 
-            report(program_state, get_file_details(path), path)
+            details = get_file_details(path)
+            if args.total != "only":
+                report(program_state, details, path)
+            # add to total
+            for k, v in details.items():
+                if k == "max_line_length":
+                    total_details[k] = max(total_details[k], v)
+                else:
+                    total_details[k] += v
+            details = {}
 
+        # report total
+        if args.total == "only":
+            report(program_state, total_details, "")
+        if args.total != "never" and (len(files) > 1 or args.total == "always"):
+            report(program_state, total_details, "total")
+        
 if __name__ == "__main__":
     main()
